c87acf4cdf379d2aba917f08a9eefec4fc467663
Veronica Bustabad <vbustabad@gmail.com>
2018-01-25 17:15:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.capitalize
+  elsif phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.downcase
+  elsif phrase == phrase.capitalize
     puts "NO, NOT SINCE 1938!"
   end
 end
